9541dedc0b3f57e800b2acc468a3a8989cf591e3
ladoc <ladochadominik@gmail.com>
2026-02-06 09:19:46 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from cafe import Cafe
-from errors import (
+from app.cafe import Cafe
+from app.errors import (
     VaccineError,
     NotWearingMaskError,
 )
@@ -12,6 +12,7 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
         try:
             cafe.visit_cafe(friend)
         except VaccineError:
+            # Jeśli ktoś ma problem ze szczepionką, od razu przerywamy
             return "All friends should be vaccinated"
         except NotWearingMaskError:
             masks_to_buy += 1
